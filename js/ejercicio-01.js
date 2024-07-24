ff1b6265c300be126966cecf862b0a5b8be0b6b2
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-23 22:51:26 -0300

    se pudo realizar el ejercicio-01 correctamente

---
+++ js/ejercicio-01.js
@@ -0,0 +1,31 @@
+const meses = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+];
+
+
+document.write('<h2>Meses del año:</h2>')
+document.write('<ul>')
+document.write(`<li>${meses[0]}</li>`)
+document.write(`<li>${meses[1]}</li>`)
+document.write(`<li>${meses[2]}</li>`)
+document.write(`<li>${meses[3]}</li>`)
+document.write(`<li>${meses[4]}</li>`)
+document.write(`<li>${meses[5]}</li>`)
+document.write(`<li>${meses[6]}</li>`)
+document.write(`<li>${meses[7]}</li>`)
+document.write(`<li>${meses[8]}</li>`)
+document.write(`<li>${meses[9]}</li>`)
+document.write(`<li>${meses[10]}</li>`)
+document.write(`<li>${meses[11]}</li>`)
+document.write('</ul>')
